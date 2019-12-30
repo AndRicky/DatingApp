309c5cdba3d014e9e8ed0312b866e57ecb9da4e8
Ricky <ricky.zhao@live.ca>
2019-12-29 22:12:22 -0500

    Expanding the Registration form

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -25,13 +25,18 @@ export class RegisterComponent implements OnInit {
     //   confirmPassword: new FormControl('', Validators.required),
     // }, this.passwordMatchValidator);
 
-    // equivalent
+    // equivalent, minus the extra fields
     this.createRegisterForm();
   }
 
   createRegisterForm() {
     this.registerForm = this.fb.group({
+      gender: ['male'],
       username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: [null, Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
       password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
       confirmPassword: ['', Validators.required]
     }, {validators: this.passwordMatchValidator});
