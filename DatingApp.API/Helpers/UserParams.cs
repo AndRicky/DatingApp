f6a65c6c8742fa2fa45a21cd3b2d56461e0465f9
Ricky <ricky.zhao@live.ca>
2019-12-30 22:38:01 -0500

    Setting up the paging helper classes

---
+++ DatingApp.API/Helpers/UserParams.cs
@@ -0,0 +1,18 @@
+namespace DatingApp.API.Helpers
+{
+    public class UserParams
+    {
+        private const int MaxPageSize = 50;
+        public int PageNumber { get; set; } = 1;
+        //default page size
+        private int pageSize = 10;
+        public int PageSize
+        {
+            get { return pageSize; }
+            //if client wants more than MaxPageSize, return MaxPageSize 
+            //other wise return what they want
+            set { pageSize = (value > MaxPageSize) ? MaxPageSize : value; }
+        }
+        
+    }
+}
