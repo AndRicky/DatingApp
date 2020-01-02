f9260750c606b50f97e302e7dd776e16051acdef
Ricky <ricky.zhao@live.ca>
2020-01-01 21:36:46 -0500

    Adding the send message functionality and a challenge

--- DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
+++ DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
@@ -3,6 +3,7 @@ import { Message } from 'src/app/_models/message';
 import { UserService } from 'src/app/_services/user.service';
 import { AuthService } from 'src/app/_services/auth.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
+import { error } from 'protractor';
 
 @Component({
   selector: 'app-member-messages',
@@ -12,18 +13,30 @@ import { AlertifyService } from 'src/app/_services/alertify.service';
 export class MemberMessagesComponent implements OnInit {
   @Input() recipientId: number;
   messages: Message[];
+  newMessage: any = {};
 
   constructor(private userService: UserService,
               private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
-    this.loadMessages()
+    this.loadMessages();
   }
 
   loadMessages() {
     this.userService.getMessageThread(this.authService.decodedToken.nameid, this.recipientId)
       .subscribe(messages => {
         this.messages = messages;
+      }, error => {
+        this.alertify.error(error);
+      });
+  }
+
+  sendMessage() {
+    this.newMessage.recipientId = this.recipientId;
+    this.userService.sendMessage(this.authService.decodedToken.nameid, this.newMessage)
+      .subscribe((message: Message) => {
+          this.messages.unshift(message);
+          this.newMessage.content = '';
       }, error => {
         this.alertify.error(error);
       });
